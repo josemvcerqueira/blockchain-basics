1fb7f31a4fe1f520aeb95170b8a83ac6ad054caf
Jose Cerqueira <jose@origensmedia.com>
2019-03-28 19:03:22 +0000

    feat: Finished the functionality of the Block class
    Built the genesis block
    Added functionality to mine a block
    Added the sha-256 function
    Used the hash to mine a block

--- components/Block/block.test.js
+++ components/Block/block.test.js
@@ -1,4 +1,6 @@
 const Block = require("./block");
+const { GENESIS_DATA } = require("../config");
+const cryptoHash = require("../cryptoHash/crypto-hash");
 
 describe("Block", () => {
 	const timestamp = "a-date";
@@ -20,11 +22,53 @@ describe("Block", () => {
 		expect(block.lastHash).toEqual(lastHash);
 	});
 
-	it("has hash property", () => {
+	it("has a hash property", () => {
 		expect(block.hash).toEqual(hash);
 	});
 
-	it("hash data property", () => {
+	it("hash a data property", () => {
 		expect(block.data).toEqual(data);
+	});
+
+	describe("genesis()", function() {
+		const genesisBlock = Block.genesis();
+
+		console.log("genesisBlock", genesisBlock);
+
+		it("returns a Block instance", () => {
+			expect(genesisBlock instanceof Block).toBe(true);
+		});
+
+		it("returns the genesis data", () => {
+			expect(genesisBlock).toEqual(GENESIS_DATA);
+		});
+	});
+
+	describe("mineBlock()", () => {
+		const lastBlock = Block.genesis();
+		const data = "mine data";
+		const minedBlock = Block.mineBlock({ lastBlock, data });
+
+		it("returns a Block Instance", () => {
+			expect(minedBlock instanceof Block).toBe(true);
+		});
+
+		it("sets the `lastHash` to be the `hash` of the lastBlock", () => {
+			expect(minedBlock.lastHash).toEqual(lastBlock.hash);
+		});
+
+		it("sets the `data`", () => {
+			expect(minedBlock.data).toEqual(data);
+		});
+
+		it("seta a `timestamp`", () => {
+			expect(minedBlock.timestamp).not.toEqual(undefined);
+		});
+
+		it("creates a SHA-256 `hash` based on the proper inputs", () => {
+			expect(minedBlock.hash).toEqual(
+				cryptoHash(minedBlock.timestamp, lastBlock.hash, data)
+			);
+		});
 	});
 });
---
+++ components/config.js
@@ -0,0 +1,8 @@
+const GENESIS_DATA = {
+	timestamp: 0,
+	lastHash: "------",
+	hash: "hash-zero",
+	data: []
+};
+
+module.exports = { GENESIS_DATA };
