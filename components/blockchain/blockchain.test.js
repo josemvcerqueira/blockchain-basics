277432b616ea730ac5601f878364bd569d18931b
Jose Cerqueira <jose@origensmedia.com>
2019-03-30 17:00:35 +0000

    feat: updated blockchain.test.js to test isValidChain method

--- components/blockchain/blockchain.test.js
+++ components/blockchain/blockchain.test.js
@@ -2,7 +2,11 @@ const Blockchain = require("./blockchain");
 const Block = require("../block/block");
 
 describe("Blockchain", () => {
-	const blockchain = new Blockchain();
+	let blockchain = new Blockchain();
+
+	beforeEach(() => {
+		blockchain = new Blockchain();
+	});
 
 	it("contains a `chain` Array instance", () => {
 		expect(blockchain.chain instanceof Array).toBe(true);
@@ -19,5 +23,50 @@ describe("Blockchain", () => {
 		expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(
 			newData
 		);
+	});
+
+	describe("isValidChain()", () => {
+		describe("when the chain does not start with the genesis block", () => {
+			it("returns false", () => {
+				blockchain.chain[0] = { data: "fake-genesis" };
+
+				expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+			});
+		});
+
+		describe("when chain starts with the genesis block and has multiple blocks", () => {
+			beforeEach(() => {
+				blockchain.addBlock({ data: "Bears" });
+				blockchain.addBlock({ data: "Lion" });
+				blockchain.addBlock({ data: "Tiger" });
+			});
+
+			describe("and a lastHash reference has changed", () => {
+				it("returns false", () => {
+					blockchain.chain[2].lastHash = "broke-lastHash";
+
+					expect(Blockchain.isValidChain(blockchain.chain)).toBe(
+						false
+					);
+				});
+			});
+
+			describe("and the chain contains a block with an invalid field", () => {
+				it("returns false", () => {
+					blockchain.chain[2].data = "corrupted data";
+					expect(Blockchain.isValidChain(blockchain.chain)).toBe(
+						false
+					);
+				});
+			});
+
+			describe("and the chain does not contain any invalid blocks", () => {
+				it("returns true", () => {
+					expect(Blockchain.isValidChain(blockchain.chain)).toBe(
+						true
+					);
+				});
+			});
+		});
 	});
 });
