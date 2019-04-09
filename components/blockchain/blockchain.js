6102f01fd1beb448fda21847b831f3a1058e9477
Jose Cerqueira <jose@origensmedia.com>
2019-04-09 15:54:09 +0100

    feat: small bug fixes to isValidChain

--- components/blockchain/blockchain.js
+++ components/blockchain/blockchain.js
@@ -33,6 +33,21 @@ class Blockchain {
 		}
 		return true;
 	}
+
+	replaceChain(chain) {
+		if (chain.length <= this.chain.length) {
+			console.error("the incoming chain must be longer");
+			return;
+		}
+
+		if (Blockchain.isValidChain(chain)) {
+			console.log("replacing chain with", chain);
+			this.chain = chain;
+		}
+
+		console.error("the incoming chain must be valid");
+		return;
+	}
 }
 
 module.exports = Blockchain;
